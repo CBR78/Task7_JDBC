093e7ba60d6a3fa43fd79895c7165d8c1cfe13b3
Malzam Vladimir <v.malzam@gmail.com>
2020-04-11 19:26:40 +0300

    refactoring (Runner methods extraction)

--- src/main/java/ua/com/foxminded/malzam/university/runner/Runner.java
+++ src/main/java/ua/com/foxminded/malzam/university/runner/Runner.java
@@ -38,23 +38,13 @@ public class Runner {
         if (selectedFunction.equals("a")) {
             System.out.println("Enter the count of students");
             int studentCount = in.nextInt();
-
-            GroupDao groupDao = new GroupDao();
-            Set<Group> groups = groupDao.searchGroups(studentCount);
-            for (Group group : groups) {
-                System.out.println(group.toString());
-            }
+            findGroupsByStudentCount(studentCount);
         }
 
         if (selectedFunction.equals("b")) {
             System.out.println("Enter course name");
             String courseName = in.next();
-
-            StudentAndCourseDao studentAndCourseDao = new StudentAndCourseDao();
-            Set<Student> students = studentAndCourseDao.showCourseStudents(courseName);
-            for (Student student : students) {
-                System.out.println(student.toString());
-            }
+            findStudentsByCourseName(courseName);
         }
 
         if (selectedFunction.equals("c")) {
@@ -62,70 +52,108 @@ public class Runner {
             String firstName = in.next();
             System.out.println("Enter student lastname");
             String lastName = in.next();
-
-            Set<Student> students = new HashSet<>();
-            students.add(new Student(firstName, lastName));
-            StudentDao studentDao = new StudentDao();
-            studentDao.addRows(students);
-
-            System.out.println("Student added");
+            addStudent(firstName, lastName);
         }
 
         if (selectedFunction.equals("d")) {
             System.out.println("Enter STUDENT_ID to remove student");
             int studentId = in.nextInt();
-
-            StudentDao studentDao = new StudentDao();
-            studentDao.removeStudent(studentId);
-
-            System.out.println("Student removed");
+            deleteStudent(studentId);
         }
 
         if (selectedFunction.equals("e")) {
-            StudentDao studentDao = new StudentDao();
-            Set<Student> students = studentDao.showAllRows();
-            for (Student student : students) {
-                System.out.println(student.toString());
-            }
-
+            showAllStudents();
             System.out.println("Enter STUDENT_ID to add the student to a course");
             int studentId = in.nextInt();
-
-            CourseDao courseDao = new CourseDao();
-            Set<Course> courses = courseDao.showAllRows();
-            for (Course course : courses) {
-                System.out.println(course.toString());
-            }
+            showAllCourses();
             System.out.println("Enter COURSE_ID to add a student to it");
             int courseId = in.nextInt();
-
-            Set<StudentAndCourse> studentsAndCourses = new HashSet<>();
-            studentsAndCourses.add(new StudentAndCourse(studentId, courseId));
-            StudentAndCourseDao studentAndCourseDao = new StudentAndCourseDao();
-            studentAndCourseDao.addRows(studentsAndCourses);
-
-            System.out.println("Student removed");
+            addStudentToCourse(studentId, courseId);
         }
 
         if (selectedFunction.equals("f")) {
             System.out.println("Enter STUDENT_ID to remove the student from a course");
             int studentId = in.nextInt();
-
-            StudentDao studentDao = new StudentDao();
-            Student student = studentDao.showRow(studentId);
-            System.out.println(student.toString());
-
-            StudentAndCourseDao studentAndCourseDao = new StudentAndCourseDao();
-            Set<Course> courses = studentAndCourseDao.showStudentCourses(studentId);
-            for (Course course : courses) {
-                System.out.println(course.toString());
-            }
-
+            showStudent(studentId);
+            showStudentcourses(studentId);
             System.out.println("Enter COURCE_ID to remove a student from the course");
             int course_id = in.nextInt();
-            studentAndCourseDao.removestudentAndCourse(studentId, course_id);
-            System.out.println("Student removed from course");
+            removeStudentFromCourse(studentId, course_id);
         }
         in.close();
+    }
+
+    private static void findGroupsByStudentCount(int studentCount) {
+        GroupDao groupDao = new GroupDao();
+        Set<Group> groups = groupDao.searchGroups(studentCount);
+        for (Group group : groups) {
+            System.out.println(group.toString());
+        }
+    }
+
+    private static void findStudentsByCourseName(String courseName) {
+        StudentAndCourseDao studentAndCourseDao = new StudentAndCourseDao();
+        Set<Student> students = studentAndCourseDao.showCourseStudents(courseName);
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
+    }
+
+    private static void addStudent(String firstName, String lastName) {
+        Set<Student> students = new HashSet<>();
+        students.add(new Student(firstName, lastName));
+        StudentDao studentDao = new StudentDao();
+        studentDao.addRows(students);
+        System.out.println("Student added");
+    }
+
+    private static void deleteStudent(int studentId) {
+        StudentDao studentDao = new StudentDao();
+        studentDao.removeStudent(studentId);
+        System.out.println("Student removed");
+    }
+
+    private static void showAllStudents() {
+        StudentDao studentDao = new StudentDao();
+        Set<Student> students = studentDao.showAllRows();
+        for (Student student : students) {
+            System.out.println(student.toString());
+        }
+    }
+
+    private static void showAllCourses() {
+        CourseDao courseDao = new CourseDao();
+        Set<Course> courses = courseDao.showAllRows();
+        for (Course course : courses) {
+            System.out.println(course.toString());
+        }
+    }
+
+    private static void addStudentToCourse(int studentId, int courseId) {
+        Set<StudentAndCourse> studentsAndCourses = new HashSet<>();
+        studentsAndCourses.add(new StudentAndCourse(studentId, courseId));
+        StudentAndCourseDao studentAndCourseDao = new StudentAndCourseDao();
+        studentAndCourseDao.addRows(studentsAndCourses);
+        System.out.println("Student added to course");
+    }
+
+    private static void showStudent(int studentId) {
+        StudentDao studentDao = new StudentDao();
+        Student student = studentDao.showRow(studentId);
+        System.out.println(student.toString());
+    }
+
+    private static void showStudentcourses(int studentId) {
+        StudentAndCourseDao studentAndCourseDao = new StudentAndCourseDao();
+        Set<Course> courses = studentAndCourseDao.showStudentCourses(studentId);
+        for (Course course : courses) {
+            System.out.println(course.toString());
+        }
+    }
+
+    private static void removeStudentFromCourse(int studentId, int course_id) {
+        StudentAndCourseDao studentAndCourseDao = new StudentAndCourseDao();
+        studentAndCourseDao.removestudentAndCourse(studentId, course_id);
+        System.out.println("Student removed from course");
     }
 }
